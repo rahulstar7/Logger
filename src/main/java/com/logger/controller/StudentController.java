47f71e2475a6b90e246a2aae20061b34e7a3c498
Rahulstar177 <rathodsachiit@gmaiil.com>
2024-09-24 18:57:18 +0530

    this is stash demo

---
+++ src/main/java/com/logger/controller/StudentController.java
@@ -0,0 +1,27 @@
+package com.logger.controller;
+
+import java.time.LocalDate;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class StudentController {
+
+private static Logger logger = LoggerFactory.getLogger(WelcomeController.class);
+	
+	@GetMapping("/welcome")
+	public String getAllLocation() {
+
+		String sMsg="Start point of getAllLocation()";
+		System.out.println(LocalDate.now()+""+sMsg);
+		logger.info(sMsg);
+		
+		String eMsg="Exit point of getAllLocation()";
+		System.out.println(LocalDate.now()+""+eMsg);
+		logger.info(eMsg);
+		return "Welcome to Bikkad IT ";
+
+	}
+
+}
